6a1fde606e833500da91ab71bec13b0c87a13fae
Soldbone <1472ezs@gmail.com>
2025-11-02 22:06:39 +0900

    feat: 입력 기능 구현
    입력 기능 추가:
    - 로또 구입 금액 입력 기능
    - 당첨 번호 입력 기능
    - 보너스 번호 입력 기능

---
+++ src/main/kotlin/lotto/constants/InputMessage.kt
@@ -0,0 +1,7 @@
+package lotto.constants
+
+enum class InputMessage(val prompt: String) {
+    PRICE("구입금액을 입력해 주세요."),
+    WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+}
---
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -0,0 +1,17 @@
+package lotto.view
+
+import camp.nextstep.edu.missionutils.Console
+import lotto.constants.InputMessage
+
+object InputView {
+    private fun prompt(message: InputMessage): String {
+        println(message.prompt)
+        return Console.readLine() ?: ""
+    }
+
+    fun readPrice(): String = prompt(InputMessage.PRICE)
+
+    fun readWinningNumber(): String = prompt(InputMessage.WINNING_NUMBER)
+
+    fun readBonusNumber(): String = prompt(InputMessage.BONUS_NUMBER)
+}
